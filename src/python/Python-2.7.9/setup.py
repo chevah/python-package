c57e3f3fb2e5082b7ab37cc39b9378ae2caf5476
Mișu Moldovan <dumol@chevah.com>
2015-02-09 16:22:11 +0200

    Extended the list of disabled python modules.

--- src/python/Python-2.7.9/setup.py
+++ src/python/Python-2.7.9/setup.py
@@ -33,7 +33,18 @@ host_platform = get_platform()
 COMPILED_WITH_PYDEBUG = ('--with-pydebug' in sysconfig.get_config_var("CONFIG_ARGS"))
 
 # This global variable is used to hold the list of modules to be disabled.
-disabled_module_list = [ 'bz2' ]
+disabled_module_list = [
+    '_bsddb',
+    '_curses',
+    '_curses_panel',
+    '_sqlite3',
+    '_tkinter',
+    'bz2',
+    'dbm',
+    'gdbm',
+    'readline',
+    'sunaudiodev',
+     ]
 
 def add_dir_to_list(dirlist, dir):
     """Add the directory 'dir' to the list 'dirlist' (at the front) if

2144e15c27265d3659295acc7aef5fa68d3e1db6
Mișu Moldovan <dumol@chevah.com>
2015-11-05 10:43:51 +0200

    Upgraded VM to OpenBSD 5.8.

--- paver.sh
+++ paver.sh
@@ -483,10 +483,10 @@ detect_os() {
         ARCH=$(uname -m)
 
         os_version_raw=$(uname -r)
-        check_os_version "OpenBSD" 5.7 "$os_version_raw" os_version_chevah
+        check_os_version "OpenBSD" 5.8 "$os_version_raw" os_version_chevah
 
-        # For now, no matter the actual OpenBSD version returned, we use '57'.
-        OS="openbsd57"
+        # For now, no matter the actual OpenBSD version returned, we use '58'.
+        OS="openbsd58"
 
     else
         echo 'Unsupported operating system:' $OS

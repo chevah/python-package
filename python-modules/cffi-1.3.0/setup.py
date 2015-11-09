927655cff0409640db943a5106a21dfa5118f278
Adi Roiban <adi.roiban@chevah.com>
2015-11-09 18:02:52 +0200

    Update cffi for static libffi linking.

--- python-modules/cffi-1.3.0/setup.py
+++ python-modules/cffi-1.3.0/setup.py
@@ -117,6 +117,12 @@ if 'freebsd' in sys.platform:
 if __name__ == '__main__':
     from setuptools import setup, Distribution, Extension
 
+    build_path = os.environ.get('CHEVAH_BUILD_PATH')
+    libffi_path = os.path.join(build_path, 'lib', 'libffi.a')
+    # We will statically link libffi.
+    libraries = []
+    extra_link_args = [libffi_path]
+
     class CFFIDistribution(Distribution):
         def has_ext_modules(self):
             # Event if we don't have extension modules (e.g. on PyPy) we want to

7415e2bbd1e68d826871008fc5f7639b65dcd109
Buildslave <buildslave@chevah.com>
2016-12-19 15:05:48 +0200

    Remove new OpenSSL includes from setup.py

--- src/python/Python-2.7.8/setup.py
+++ src/python/Python-2.7.8/setup.py
@@ -46,7 +46,7 @@ disabled_module_list = [
     ]
 
 # Compile the readline module only on platforms whitelisted below.
-if host_platform not in ('linux2', 'sunos5' ):
+if host_platform not in ('linux2'):
     disabled_module_list.append('readline')
 
 def add_dir_to_list(dirlist, dir):
@@ -803,7 +803,7 @@ class PyBuildExt(build_ext):
                                libraries=math_libs) )
         # Detect SSL support for the socket module (via _ssl)
         search_for_ssl_incs_in = [
-                              '/usr/local/ssl/include',
+                              #'/usr/local/ssl/include',
                               '/usr/sfw/include/',
                               '/usr/contrib/ssl/include/'
                              ]
@@ -829,8 +829,8 @@ class PyBuildExt(build_ext):
                                          [ '/usr/sfw/lib' ] )
         else:
             ssl_libs = find_library_file(self.compiler, 'ssl',lib_dirs,
-                                     ['/usr/local/ssl/lib',
-                                      '/usr/contrib/ssl/lib/'
+                                     #['/usr/local/ssl/lib',
+                                     ['/usr/contrib/ssl/lib/'
                                      ] )
 
         if (ssl_incs is not None and

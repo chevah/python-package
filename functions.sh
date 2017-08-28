a3cb2b955e945079964c29c71e3efe9c6ba47473
dumol <misu.moldovan@gmail.com>
2017-08-28 15:25:50 +0300

    [#4299] HP-UX processor count fix. (#87)
    * Improved comments for CPU enumeration.
    
    * Count logical cores in HP-UX.
    
    * More comments for CPU enumeration.
    
    * Work around the GMP failure to build on HP-UX with lots of threads.
    
    * Changes after own review.
    
    * More comment changes after own review.

--- functions.sh
+++ functions.sh
@@ -241,28 +241,36 @@ wipe_manifest() {
 
 }
 
+#
+# Get number of CPUs on supported OS'es.
+#
 get_number_of_cpus() {
     case "$OS" in
         windows*)
+            # Logical CPUs (including hyper-threading) in Windows 2000 or newer.
             CPUS="$NUMBER_OF_PROCESSORS"
             ;;
         aix*)
-            # This works in an AIX 5.3 vWPAR too.
-            CPUS=$(lparstat -i | grep ^"Maximum Physical CPUs" | cut -d\: -f2)
+            # Physical CPUs on AIX 5.3/6.1/7.1, including (v)WPARs.
+            # CPU threads don't help us on PPC64 with this workload.
+            CPUS=$(lparstat -i | grep ^"Active Physical CPUs" | cut -d\: -f2)
             ;;
         solaris*)
-            # Only count physical processors. Tested on SPARC, AMD64 and X86.
+            # Physical CPUs. SPARC has lots of threads lately, but they don't
+            # help much here. Tested on Solaris 10/11 on X86/AMD64/SPARC.
             CPUS=$(/usr/sbin/psrinfo -p)
             ;;
         hpux*)
-            # Only count physical processors. Tested on Itanium.
-            CPUS=$(machinfo | grep proc | grep core | awk '{print $1}')
+            # Logical CPUs. Tested on HP-UX 11.31 running on Itaniums.
+            CPUS=$(ioscan -kFC processor | wc -l)
             ;;
         osx*|macos*|freebsd*|openbsd*|netbsd*)
+            # Logical CPUs.
             CPUS=$(sysctl -n hw.ncpu)
             ;;
         *)
             # Only Linux distros should be left.
+            # Logical CPUS.
             # Don't use lscpu/nproc or other stuff not present on older distros.
             CPUS=$(getconf _NPROCESSORS_ONLN)
             ;;

68a3132f8fd1e99a60ea412c2bc491b843c4327f
dumol <dumol@chevah.com>
2018-05-04 10:03:52 +0000

    [#4837] cryptography updates. (#102)
    * Use latest possible cryptography on RHEL5/SLES11.
    
    * Updated cryptography to 2.2.2 (with OpenSSL 1.1.0h on Windows/MacOS).
    
    * Updated pip and setuptools to latest stable versions.
    
    * Fixed OpenBSD build.
    
    * Added latest cffi, version 1.11.5.
    
    * Happily hack in support for OpenBSD 6.3.
    
    * Updated paver.conf to support FreeBSD 10 and OpenBSD 6.3.
    
    * Updated SQLite to 3.23.1.
    
    * More comments for Solaris and OpenBSD.
    
    * Updated scandir to 1.7.
    
    * Updated scandir-related tests.
    
    * Call ioscan with full path on HP-UX.
    
    * Use cffi-based modules on HP-UX with cryptography 1.8.2.
    
    * Updated paver.conf for Ubuntu 18.04.
    
    * Use cffi-based modules on AIX with latest cryptography.
    
    * Use latest cryptography on HP-UX.
    
    * Use cffi-based modules on Solaris 11.2 with cryptography 1.8.2.
    
    * Removed cffi 1.5.2 sources.
    
    * A more elegant fix for ld_so_aix missing.
    
    * Cosmetic changes after review.

--- python-modules/chevah-python-test/test_python_binary_dist.py
+++ python-modules/chevah-python-test/test_python_binary_dist.py
@@ -372,8 +372,8 @@ def get_allowed_deps():
         allowed_deps = [
             '/usr/lib/libc.so',
             '/usr/lib/libcrypto.so',
+            '/usr/lib/libcurses.so',
             '/usr/lib/libm.so',
-            '/usr/lib/libncursesw.so',
             '/usr/lib/libpthread.so',
             '/usr/lib/libssl.so',
             '/usr/lib/libutil.so',
@@ -565,9 +565,9 @@ def main():
             print 'cryptography %s - OpenSSL %s' % (
                 cryptography.__version__, openssl_version)
 
-            if chevah_os == 'windows':
-                # Check OpenSSL version on windows.
-                expecting = u'OpenSSL 1.1.0f  25 May 2017'
+            if chevah_os in [ "windows", "macos1012" ]:
+                # Check OpenSSL version from upstream wheels.
+                expecting = u'OpenSSL 1.1.0h  27 Mar 2018'
                 if openssl_version != expecting:
                     sys.stderr.write('Expecting %s got %s.\n' % (
                         expecting, openssl_version))

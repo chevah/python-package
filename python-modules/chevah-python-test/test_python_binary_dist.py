a5f30c671f84848c6d4d5ea058a9949246c2261e
dumol <dumol@gnome.org>
2019-04-02 19:36:44 +0300

    Removed psutil test exception on Windows.

--- python-modules/chevah-python-test/test_python_binary_dist.py
+++ python-modules/chevah-python-test/test_python_binary_dist.py
@@ -861,10 +861,7 @@ def main():
             exit_code = 18
 
     # Some OS'es are not supported by upstream psutil.
-    # FIXME:5185:
-    # Windows is special. We build python-package on XP, with psutil as a wheel,
-    # but XP/2003 not supported any more upstream, so we can't test for it.
-    if not chevah_os in [ 'aix53', 'hpux1131', 'windows' ]:
+    if not chevah_os in [ 'aix53', 'hpux1131' ]:
         try:
             import psutil
             print 'psutil %s' % (psutil.__version__,)

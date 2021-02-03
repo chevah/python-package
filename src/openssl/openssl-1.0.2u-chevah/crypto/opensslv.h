1561a11a1850c54042c1c31a5861822dc27f1811
dumol <dumol@gnome.org>
2021-02-03 11:21:46 +0200

    Give our OpenSSL release a proper name.

--- src/openssl/openssl-1.0.2u-chevah/crypto/opensslv.h
+++ src/openssl/openssl-1.0.2u-chevah/crypto/opensslv.h
@@ -32,9 +32,9 @@ extern "C" {
  */
 # define OPENSSL_VERSION_NUMBER  0x10002160L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-fips-dev  xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-fips-chevah  03 Feb 2021"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-dev  xx XXX xxxx"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-chevah  03 Feb 2021"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 

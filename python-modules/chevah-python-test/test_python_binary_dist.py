27c938f781b826b58b632183e1947e8985ee3a7c
dumol <dumol@gnome.org>
2022-01-21 13:24:40 +0200

    Updated built/test scripts for OpenSSL 1.1.1m.

--- python-modules/chevah-python-test/test_python_binary_dist.py
+++ python-modules/chevah-python-test/test_python_binary_dist.py
@@ -504,7 +504,7 @@ def main():
         openssl_version = backend.openssl_version_text()
         if CHEVAH_OS.startswith(("win", "lnx", "macos", "aix")):
             # On some OS'es we build against our own OpenSSL.
-            expecting = u'OpenSSL 1.1.1l  24 Aug 2021'
+            expecting = u'OpenSSL 1.1.1m  14 Dec 2021'
             if CHEVAH_OS.startswith("aix"):
                 # On AIX we are stuck with a patched 1.0.2.
                 expecting = u'OpenSSL 1.0.2v-chevah3  15 Sep 2021'

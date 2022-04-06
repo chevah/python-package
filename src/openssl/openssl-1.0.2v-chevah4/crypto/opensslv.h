0e69f526d0519932d064c3da0059f33a86980172
dumol <dumol@gnome.org>
2022-04-06 14:27:34 +0300

    Updated OpenSSL 1.0.2 sources to version 1.0.2v-chevah4.

--- src/openssl/openssl-1.0.2v-chevah4/crypto/opensslv.h
+++ src/openssl/openssl-1.0.2v-chevah4/crypto/opensslv.h
@@ -32,9 +32,9 @@ extern "C" {
  */
 # define OPENSSL_VERSION_NUMBER  0x10002160L
 # ifdef OPENSSL_FIPS
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-fips-chevah3  15 Sep 2021"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-fips-chevah4  06 Apr 2022"
 # else
-#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-chevah3  15 Sep 2021"
+#  define OPENSSL_VERSION_TEXT    "OpenSSL 1.0.2v-chevah4  06 Apr 2022"
 # endif
 # define OPENSSL_VERSION_PTEXT   " part of " OPENSSL_VERSION_TEXT
 

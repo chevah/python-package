e65a5fe3938e17bbcbf88848eb82ebaef7f3177d
dumol <dumol@gnome.org>
2022-09-08 12:20:41 +0300

    Updated list of RHEL clones from server repo.

--- brink.sh
+++ brink.sh
@@ -762,7 +762,7 @@ detect_os() {
                 # Some rolling-release distros (eg. Arch Linux) have
                 # no VERSION_ID here, so don't count on it unconditionally.
                 case "$linux_distro" in
-                    rhel|centos|ol)
+                    rhel|centos|almalinux|rocky|ol)
                         os_version_raw="$VERSION_ID"
                         check_os_version "Red Hat Enterprise Linux" 8 \
                             "$os_version_raw" os_version_chevah

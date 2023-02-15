cc62314098ad317c83db69595eef5d7f5daa1ad8
dumol <1620933+dumol@users.noreply.github.com>
2023-02-15 13:06:25 +0200

    OpenSSL 1.1.1 and Python 2.7 security updates. (#174)

--- python-modules/chevah-python-test/test_python_binary_dist.py
+++ python-modules/chevah-python-test/test_python_binary_dist.py
@@ -433,7 +433,7 @@ def main():
         openssl_version = backend.openssl_version_text()
         if CHEVAH_OS.startswith(("win", "lnx", "macos", "aix")):
             # On some OS'es we build against our own OpenSSL.
-            expecting = u'OpenSSL 1.1.1s  1 Nov 2022'
+            expecting = u'OpenSSL 1.1.1t  7 Feb 2023'
             if CHEVAH_OS.startswith("aix"):
                 # On AIX we are stuck with a patched 1.0.2.
                 expecting = u'OpenSSL 1.0.2v-chevah4  06 Apr 2022'

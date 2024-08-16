c01cfeff3babb5b97b6f33b104256d891fc71891
dumol <dumol@gnome.org>
2024-08-16 08:54:10 +0000

    Patch CVE-2023-49083 for cryptography 3.2.1 the same way as for CVE-2023-23931.

--- python-modules/cryptography-3.2.1/src/cryptography/hazmat/backends/openssl/backend.py
+++ python-modules/cryptography-3.2.1/src/cryptography/hazmat/backends/openssl/backend.py
@@ -2673,12 +2673,9 @@ class Backend(object):
                 _Reasons.UNSUPPORTED_SERIALIZATION,
             )
 
-        certs = []  # type: list of x509.Certificate
-        if p7.d.sign == self._ffi.NULL:
-            return certs
-
         sk_x509 = p7.d.sign.cert
         num = self._lib.sk_X509_num(sk_x509)
+        certs = []
         for i in range(num):
             x509 = self._lib.sk_X509_value(sk_x509, i)
             self.openssl_assert(x509 != self._ffi.NULL)
